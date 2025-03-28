af64013fa2a199a447fe3e17a7f43a998a28eea9
Jarek Gadzinski <jaroslaw.gadzinski@lizardmedia.pl>
2025-03-28 07:50:14 +0100

    Fix route middleware group to match Krayin admin

--- src/Routes/admin-routes.php
+++ src/Routes/admin-routes.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Puzmiki\LeadPipelineSwitcher\Http\Controllers\PipelineSwitcherController;
 
-Route::group(['middleware' => ['web', 'admin']], function () {
-    Route::post('admin/leads/{id}/switch-pipeline', [PipelineSwitcherController::class, 'switch'])->name('admin.leads.switch-pipeline');
+Route::group([
+    'middleware' => ['web', 'admin_locale', 'user'],
+    'prefix'     => config('app.admin_path'),
+], function () {
+    Route::post('leads/view/{id}/switch-pipeline', [
+        'uses' => 'Puzmiki\LeadPipelineSwitcher\Http\Controllers\PipelineSwitcherController@switch',
+        'as'   => 'admin.leads.switch-pipeline',
+    ]);
 });
